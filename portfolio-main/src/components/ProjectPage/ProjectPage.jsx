909f9fa86179ea197d0ae20bf35334c77df51385
sprisca <priscasaina5@gmail.com>
2024-12-11 19:38:58 +0100

    MODIFICATION DES scss

--- portfolio-main/src/components/ProjectPage/ProjectPage.jsx
+++ portfolio-main/src/components/ProjectPage/ProjectPage.jsx
@@ -1,38 +1,64 @@
 import { useParams } from "react-router-dom";
 import projects from "../../data/projects.json";
-import { Link } from "react-router-dom";
-const findProjectID = (id) => {
+
+const findProjectByID = (id) => {
   return projects.find((project) => project.id === id);
 };
-console.log(projects);
+
 const ProjectPage = () => {
   const { id } = useParams();
-  const project = findProjectID(id);
-  const technologies =project.technologies;
-  console.log(project);
-  console.log("voici l'id:", id);
+  const project = findProjectByID(id);
+
+  // Gérer l'absence de projet
+  if (!project) {
+    return (
+      <section className="ContainerProjectPage">
+        <h1>Projet introuvable</h1>
+        <p>Le projet avec l'ID "{id}" n'existe pas ou a été supprimé.</p>
+      </section>
+    );
+  }
+
+  const { title, description, technologies, github, site, img_l, img_r } = project;
+
   return (
     <section className="ContainerProjectPage">
       <div className="projectPage">
-        <h1>{project.title}</h1>
-        <h2>Description:</h2>
-          <p>{project.description}</p> 
+        <h1>{title}</h1>
+        <h2>Description :</h2>
+        <p>{description}</p>
+
+        <h3>Technologies :</h3>
         <ul>
-          {technologies.map((techno,index)=>(
-            <li key={("techno",index)}>{techno}</li>
+          {technologies.map((techno, index) => (
+            <li key={index}>{techno}</li>
           ))}
         </ul>
+
         <div className="links">
-          <Link to={project.github} target="_blank">Code</Link>
-          <Link to={project.site} target="_blank">Site web</Link>
+          {github && (
+            <a href={github} target="_blank" rel="noopener noreferrer">
+              Code
+            </a>
+          )}
+          {site && (
+            <a href={site} target="_blank" rel="noopener noreferrer">
+              Site web
+            </a>
+          )}
         </div>
+
         <div className="container-img">
-          <div className="img-L">
-            <img src={project.img_l} alt={project.title} />
-          </div>
-          <div className="img-R">
-            <img src={project.img_r} alt={project.title} />
-          </div>
+          {img_l && (
+            <div className="img-L">
+              <img src={img_l} alt={`Aperçu gauche de ${title}`} />
+            </div>
+          )}
+          {img_r && (
+            <div className="img-R">
+              <img src={img_r} alt={`Aperçu droit de ${title}`} />
+            </div>
+          )}
         </div>
       </div>
     </section>
